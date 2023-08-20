d39887ffe931797b009bade376f80f38540adbfa
Midomik <krastikrabbs9234@gmail.com>
2023-08-20 16:11:01 +0300

    comm 5

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -12,7 +12,7 @@ function renderGalery(imgObject) {
     const renderPhoto = imgObject.map(item=>{ 
         return `<li class="gallery__item">
         <a class="gallery__link" href="${item.original}"> 
-        <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+        <img class="gallery__image lazyload" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
         </a>
         </li>`
     }).join("");
@@ -26,17 +26,28 @@ renderGalery(galleryItems);
 
 const openOriginPhoto = (e) => {
     e.preventDefault();
-
     if (e.currentTarget === e.target){
          return;
     }
 
 
     const instance = basicLightbox.create(`
-        <img src="${e.target.dataset.source}" width="800" height="600">
+        <img claas="lazyload" src="${e.target.src}" data-src="${e.target.dataset.source}" width="800" height="600">
     `)
-    instance.show()
+    instance.show();
     
+    
+    const outputValue = (event) => {
+        if (event.code==="Escape"){
+            instance.close();
+        }
+    }
+    
+    document.addEventListener("keydown", outputValue)
+
 }
+
+
+
 
 galaryList.addEventListener("click", openOriginPhoto);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galaryList = document.querySelector(".gallery");
+
+function renderGalery(imgObject) {
+    const renderPhoto = imgObject.map(item=>{ 
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}"> 
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+        </li>`
+    }).join("");
+
+    galaryList.insertAdjacentHTML('afterbegin', renderPhoto);
+
+}
+
+renderGalery(galleryItems);
+
+
+// function renderSwiperGalery(imgObject){
+//     const renderSwiperPhoto = imgObject.map(item=>{ 
+//         return `<div class="gallery">
+//         <a href=""><img src="" alt="" title=""></a>
+//         </div>`
+//     }).join("");
+    
+// }
+
+
+
+const openSwiper =(e) =>{
+    e.preventDefault();
+    if (e.currentTarget === e.target){
+        return;
+   }
+   console.log("lll")
+
+   var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+}
+
+galaryList.addEventListener("click", openSwiper)
